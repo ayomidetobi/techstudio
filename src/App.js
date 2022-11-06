420bd01878a5c4d7049630a7a5bf78afe920b50b
ayomidetobi <ayomidestobsy10@gmail.com>
2022-11-06 13:02:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Course from './components/Course';
+import './helper.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container-fluid'>
+      <Course />
+      </div>
+      
     </div>
   );
 }
